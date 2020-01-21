7f2bcaa4b6c2219a69befa8a2c5d913c5631372f
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2020-01-21 20:53:30 +0200

    Get rid of the precompiled headers.

--- App.cpp
+++ App.cpp
@@ -1,4 +1,10 @@
-#include "pch.h"
+#include <windows.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.ApplicationModel.Core.h>
+#include <winrt/Windows.UI.Core.h>
+#include <winrt/Windows.UI.Composition.h>
+#include <winrt/Windows.UI.Input.h>
 
 using namespace winrt;
 
