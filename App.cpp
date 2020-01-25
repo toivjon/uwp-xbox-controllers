cee9cea229a39ba38150961aeec84abca442ca5d
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2020-01-25 09:40:14 +0200

    Added the base project files for a C++/CX UWP application.

---
+++ App.cpp
@@ -0,0 +1,42 @@
+﻿using namespace Windows::ApplicationModel::Core;
+using namespace Windows::UI::Core;
+using namespace Platform;
+
+ref class App sealed : public IFrameworkView, IFrameworkViewSource
+{
+public:
+	virtual void Initialize(CoreApplicationView^ applicationView) {
+
+	}
+		
+	virtual void SetWindow(CoreWindow^ window) {
+
+	}
+
+	virtual void Load(String^ entryPoint) {
+
+	}
+		
+	virtual void Run() {
+		auto window = CoreWindow::GetForCurrentThread();
+		window->Activate();
+
+		auto dispathcher = window->Dispatcher;
+		dispathcher->ProcessEvents(CoreProcessEventsOption::ProcessUntilQuit);
+	}
+
+	virtual void Uninitialize() {
+
+	}
+
+	virtual IFrameworkView^ CreateView() {
+		return ref new App();
+	}
+};
+
+[MTAThread]
+int main(Array<String^>^)
+{
+	CoreApplication::Run(ref new App());
+	return 0;
+}
