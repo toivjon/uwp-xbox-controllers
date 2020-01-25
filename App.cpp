3a529202943d2e9e8a86d27e30c573f7e8a08a6b
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2020-01-25 10:27:25 +0200

    Added a support to show toasts.

--- App.cpp
+++ App.cpp
@@ -1,12 +1,29 @@
-﻿using namespace Windows::ApplicationModel::Core;
+using namespace Windows::ApplicationModel::Core;
+using namespace Windows::Gaming::Input;
+using namespace Windows::Foundation;
 using namespace Windows::UI::Core;
+using namespace Collections;
 using namespace Platform;
+
+using namespace Windows::Data::Xml::Dom;
+using namespace Windows::UI::Notifications;
 
 ref class App sealed : public IFrameworkView, IFrameworkViewSource
 {
 public:
 	virtual void Initialize(CoreApplicationView^ applicationView) {
+		// initialize the support to show toast messages.
+		mToastNotifier = ToastNotificationManager::CreateToastNotifier();
 
+		// add a listener to track whenever a gamepad gets added.
+		Gamepad::GamepadAdded += ref new EventHandler<Gamepad^>([=](Object^, Gamepad^ gamepad) {
+			int foobar = 0;
+		});
+
+		// add a listener to track whenever a gamepad gets removed.
+		Gamepad::GamepadRemoved += ref new EventHandler<Gamepad^>([=](Object^, Gamepad^ gamepad) {
+
+		});
 	}
 		
 	virtual void SetWindow(CoreWindow^ window) {
@@ -21,6 +38,8 @@ public:
 		auto window = CoreWindow::GetForCurrentThread();
 		window->Activate();
 
+		ShowToast("topic!", "description!");
+
 		auto dispathcher = window->Dispatcher;
 		dispathcher->ProcessEvents(CoreProcessEventsOption::ProcessUntilQuit);
 	}
@@ -32,6 +51,20 @@ public:
 	virtual IFrameworkView^ CreateView() {
 		return ref new App();
 	}
+
+	void ShowToast(String^ topic, String^ content) {
+		// create toast contents in XML.
+		auto xml = ToastNotificationManager::GetTemplateContent(ToastTemplateType::ToastText02);
+		auto textNodes = xml->GetElementsByTagName("text");
+		textNodes->GetAt(0)->AppendChild(xml->CreateTextNode(topic));
+		textNodes->GetAt(1)->AppendChild(xml->CreateTextNode(content));
+
+		// create a toast and show it!
+		auto toast = ref new ToastNotification(xml);
+		mToastNotifier->Show(toast);
+	}
+private:
+	ToastNotifier^ mToastNotifier;
 };
 
 [MTAThread]
